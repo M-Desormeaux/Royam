5fc4c6778125dc72604658cc4c33d7c9d2e94057
Michael Desormeaux <mdesormeaux96@gmail.com>
2019-10-15 21:51:35 -0500

    Update

--- cogs/admin.py
+++ cogs/admin.py
@@ -6,13 +6,58 @@ class Admin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+# {{{ Hidden On_Ready
 #   @commands.Cog.listener()
 #   async def on_ready(self):
 #       print("=> admin")
+# }}}
 
+# {{{ Ping
     @commands.command()
     async def ping(self, ctx):
         await ctx.send('Pong! 🏓')
+# }}}
+
+# {{{ Clear
+    @commands.command()
+    async def clear(self, ctx, amount=5):
+        await ctx.channel.purge(limit=amount+1)
+# }}}
+
+# {{{ Kick
+    @commands.command()
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        if reason is None:
+            await ctx.send(f'{member.mention} was kicked')
+        else:
+            await ctx.send(f'{member.mention} was kicked for {reason}')
+# }}}
+
+# {{{ Ban
+    @commands.command()
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        if reason is None:
+            await ctx.send(f'{member.mention} was banned')
+        else:
+            await ctx.send(f'{member.mention} was banned for {reason}')
+# }}}
+
+# {{{ Unban
+    @commands.command()
+    async def unban(self, ctx, member: discord.Member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'{user.mention} unbanned')
+                return
+# }}}
 
 
 def setup(cog):
--- core.py
+++ core.py
@@ -8,10 +8,12 @@ bot = commands.Bot(command_prefix='.')
 # {{{ On_Ready
 @bot.event
 async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="The Server"))
     print("Bot ready to interact")
 # }}}
 
 # {{{ Cogs
+# {{{ Load Cog
 @bot.command()
 async def load(ctx, extension):
     try:
@@ -20,8 +22,8 @@ async def load(ctx, extension):
         print(f'> Cog Loaded: {extension}')
     except:
         await ctx.send('Cog not found')
-
-
+# }}}
+# {{{ Unload Cog
 @bot.command()
 async def unload(ctx, extension):
     try:
@@ -30,8 +32,8 @@ async def unload(ctx, extension):
         print(f'> Cog UnLoaded: {extension}')
     except:
         await ctx.send('Cog not found')
-
-
+# }}}
+# {{{ Reload Cog
 @bot.command()
 async def reload(ctx, extension):
     try:
@@ -41,11 +43,34 @@ async def reload(ctx, extension):
         print(f'> Cog Reloaded: {extension}')
     except:
         await ctx.send('Cog not found')
-
-
+# }}}
+# {{{ Cycle Cogs
+@bot.command()
+async def cycle(ctx):
+    await ctx.send('Reloading Cogs...')
+    print('Reloading all cogs')
+    try:
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                bot.unload_extension(f'cogs.{filename[:-3]}')
+    except:
+        pass
+    try:
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                bot.load_extension(f'cogs.{filename[:-3]}')
+                print(f'> Cog Loaded: {filename[:-3]}')
+    except:
+        pass
+    await ctx.send('Cogs Reloaded')
+# }}}
+# {{{ Pre-Load
+print('\nCogs Loaded:')
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
-        print(f'> Cog Loaded: {filename[:-3]}')
+        print(f'{filename[:-3]}')
+print('')
+# }}}
 # }}}
 bot.run(token.strip())
